897f7230a9dc5b307c6fa751481ee7ead8dbe80a
alyssakelley <abkelley97@gmail.com>
2020-06-05 23:56:04 -0700

    Updates

---
+++ precache-manifest.6d7e42225cf68b716d94844cd579b163.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22e29f09667937ea414e4840b52cb2a4",
+    "url": "/covid19_visualization/index.html"
+  },
+  {
+    "revision": "41e296f123ae366faebf",
+    "url": "/covid19_visualization/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "284ad2ff19139886dfe3",
+    "url": "/covid19_visualization/static/css/main.6d35838e.chunk.css"
+  },
+  {
+    "revision": "41e296f123ae366faebf",
+    "url": "/covid19_visualization/static/js/2.409d2673.chunk.js"
+  },
+  {
+    "revision": "b1af44c7d782d8cd3b71e08e7c324f6e",
+    "url": "/covid19_visualization/static/js/2.409d2673.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "284ad2ff19139886dfe3",
+    "url": "/covid19_visualization/static/js/main.a5bd3bab.chunk.js"
+  },
+  {
+    "revision": "e6387381b3c06953244a",
+    "url": "/covid19_visualization/static/js/runtime-main.0a497851.js"
+  },
+  {
+    "revision": "5255d57ebe9388ea934b341ec84f5cff",
+    "url": "/covid19_visualization/static/media/covid19_image2.5255d57e.png"
+  },
+  {
+    "revision": "5c1adb944b6f28a6fa859c8d3511a8e5",
+    "url": "/covid19_visualization/static/media/sym_breathing.5c1adb94.png"
+  },
+  {
+    "revision": "99315a68712fb7d7dc377811dd9de558",
+    "url": "/covid19_visualization/static/media/sym_cough.99315a68.png"
+  },
+  {
+    "revision": "560e5289680049d8788a72f7d6b0d351",
+    "url": "/covid19_visualization/static/media/sym_fever.560e5289.png"
+  },
+  {
+    "revision": "60a40790dea7b504020c451b260dc013",
+    "url": "/covid19_visualization/static/media/th1_breathing.60a40790.png"
+  },
+  {
+    "revision": "9df417b6bc72409f7193b3c46aba1e37",
+    "url": "/covid19_visualization/static/media/th1_temp.9df417b6.png"
+  },
+  {
+    "revision": "65410af54ff46e06d5a3e6355fb1589b",
+    "url": "/covid19_visualization/static/media/th1_washhands.65410af5.png"
+  }
+]);
